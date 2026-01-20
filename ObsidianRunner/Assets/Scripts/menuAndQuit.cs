5c9d01dd8c8e15a00228b98761dff33065e2c0f6
vanshkanaiya <vanshkanaiyaok@gmail.com>
2026-01-20 12:37:49 +0530

    Menu Button  Script Added

---
+++ ObsidianRunner/Assets/Scripts/menuAndQuit.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menuAndQuit : MonoBehaviour
+{
+    public void playGame() {
+        SceneManager.LoadScene(1);
+    }
+
+    public void quitGame() {
+        Debug.Log("Game quit");
+        Application.Quit();
+    }
+}
