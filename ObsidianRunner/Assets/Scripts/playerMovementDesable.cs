2682b0486bd4339083bdb5b5a408414641d66ffa
SohamGohil14812 <sohamgohil69@gmail.com>
2026-04-01 15:02:43 +0530

    player movement disable

---
+++ ObsidianRunner/Assets/Scripts/playerMovementDesable.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovementDesable : MonoBehaviour
+{
+    public playerMovement playerMovement;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Enemy")
+        {
+            GetComponent<playerMovement>().enabled = false;
+        }
+        if (collision.tag == "End")
+        {
+            GetComponent<playerMovement>().enabled = false;
+        }
+    }
+}
