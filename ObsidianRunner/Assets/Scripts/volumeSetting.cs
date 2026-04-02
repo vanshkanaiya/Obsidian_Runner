48d8276631690d51251c182e329ddd54b0b8e2de
SohamGohil14812 <sohamgohil69@gmail.com>
2026-04-02 18:26:47 +0530

    setting ui

---
+++ ObsidianRunner/Assets/Scripts/volumeSetting.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class volumeSetting : MonoBehaviour
+{
+    [SerializeField] Slider volumeSlider;
+    [SerializeField] Slider music  Slider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
